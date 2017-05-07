8ed5cd8870af35f455f5f1efbcf6552d79a4239f
SaddamAkbar IAKCollaborationLayout <reesad.phx89@gmail.com>
2017-05-07 16:38:21 +0700

    Animalium Apps

---
+++ app/src/main/java/iak/saddamakbar/animalium/AnimalModel.java
@@ -0,0 +1,35 @@
+package iak.saddamakbar.animalium;
+
+/**
+ * Created by SAHFTECH on 5/7/2017.
+ */
+
+public class AnimalModel {
+
+    String namaIndo, namaEng;
+    int gambar;
+
+    public String getNamaIndo() {
+        return namaIndo;
+    }
+
+    public String getNamaEng() {
+        return namaEng;
+    }
+
+    public int getGambar() {
+        return gambar;
+    }
+
+    public AnimalModel(String namaIndo, String namaEng, int gambar){
+
+        this.namaIndo = namaIndo;
+        this.namaEng = namaEng;
+        this.gambar = gambar;
+
+
+
+
+    }
+
+}
--- app/src/main/java/iak/saddamakbar/animalium/FrontActivity.java
+++ app/src/main/java/iak/saddamakbar/animalium/FrontActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FrontActivity extends AppCompatActivity {
 
@@ -13,12 +14,17 @@ public class FrontActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
 
+        final EditText EdtNama = (EditText) findViewById(R.id.meditTextNama);
+
 
         Button BtnMasuk = (Button) findViewById(R.id.btnMasuk);
         BtnMasuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent masuk = new Intent(FrontActivity.this, MainActivity.class);
+                masuk.putExtra("NAMA", EdtNama.getText().toString());
+
+
                 startActivity(masuk);
             }
         });
